de1b5b313d472833ed75e9810b9bfca30a5dede6
christopher-hague <christopher.a.hague@gmail.com>
2017-11-29 13:30:48 -0600

    enabled create table for game board

--- trivia-boys-client/src/components/FiveLetterWord.js
+++ trivia-boys-client/src/components/FiveLetterWord.js
@@ -23,7 +23,7 @@ class FiveLetterWord extends React.Component {
         alert("Error")
       } else {
         this.setState({
-          words: this.shuffle(json).slice(0, 99)
+          words: this.shuffle(json).slice(0, 100)
         })
       }
     })
@@ -42,34 +42,38 @@ class FiveLetterWord extends React.Component {
   // if (i + 1) % 10 === 0, then we are at the tenth <td> in the <tr> and can close the <tr>
   // else make a <td> for the word
 
+  createWordsRows() {
+    // wrap each element in a <tr> tag, then wrap each element of the subArr in a <td> tag
+    return [
+      this.state.words.slice(0,10).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(10,20).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(20,30).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(30,40).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(40,50).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(50,60).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(60,70).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(70,80).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(80,90).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>),
+      this.state.words.slice(90,101).map(word => <td className="TextBox" key={word.text}><TextBox word={word.text} /></td>)
+    ]
+  }
+
+  createBoard() {
+    return this.createWordsRows().map((wordRow, i) => <tr key={i}>{wordRow}</tr>)
+  }
 
   makeButton(word, i) {
     return <td className="TextBox" key={i}><TextBox word={word.text} /></td>
   }
 
-  populateTableRows() {
-    var result = []
-    for(var i = 0; i < this.state.words.length; i++) {
-      result.push(<td className="TextBox" key={i}><TextBox word={this.state.words[i].text} /></td>)
-    }
-    return result
-  }
-
-  generateTableRows() {
-    var result = []
-    for(var i = 0; i < 10; i++) {
-      result.push(<tr key={i}>{this.populateTableRows()}</tr>)
-    }
-    return result
-  }
-
   render() {
     console.log("State: ", this.state)
     console.log("Props: ", this.props)
+    console.log(this.createWordsRows())
     if(!this.state.words) {
       return null
     } else {
-      var gameBoard = this.generateTableRows()
+      var gameBoard = this.createBoard()
 
       return(
         <div className="FiveLetterWord">
